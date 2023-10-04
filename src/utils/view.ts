47a4e8568d1784e503ee3cfb33903e9fcc4054c2
AnastasiyaAlisenok <119356164+AnastasiyaAlisenok@users.noreply.github.com>
2023-10-04 10:55:28 +0300

    Update view.ts

--- src/utils/view.ts
+++ src/utils/view.ts
@@ -1,1 +1,15 @@
+import ElementCreator from './elementCreator';
+import { ElementsArgs } from '../types/types';
+
+export default class View {
+    public viewElementCreator: ElementCreator;
+
+    constructor(args: ElementsArgs) {
+        this.viewElementCreator = new ElementCreator(args);
+    }
+
+    public getHtmlElement(): HTMLElement | null {
+        return this.viewElementCreator.getElement();
+    }
+}
 
